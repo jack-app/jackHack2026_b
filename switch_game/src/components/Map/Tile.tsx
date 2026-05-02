b3b6dc08fafc964498986711245fed2da5612b33
butterfly <shuwei889@gmail.com>
2026-05-02 17:49:56 +0900

    タイルの実装

--- switch_game/src/components/Map/Tile.tsx
+++ switch_game/src/components/Map/Tile.tsx
@@ -5,9 +5,23 @@ interface TileProps {
 }
 
 export default function Tile({ cell, switchState, switchWeight }: TileProps) {
+
+  const wall = cell === 0;
+  const floor = cell === 1;
+  const switch = typeof cell === "string" && cell.startsWith("s");
+
+  const base =  "w-10 h-10 flex items-center justify-center text-white font-bold";
+  const wall = 
+
+
   return (
-    <div>
+    <div className="text-sm">
       {/* TODO: implement - 0: wall, 1: floor, "sXX": switch */}
+
+      <div className="">
+        
+      </div>
+
     </div>
   );
 }
