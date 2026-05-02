5cede0bfbca3959e8b8d939cb17645c01978e615
kumasan <tateru99@gmail.com>
2026-05-02 17:09:04 +0900

    ResultModal UI実装

--- switch_game/src/components/UI/ResultModal.tsx
+++ switch_game/src/components/UI/ResultModal.tsx
@@ -9,8 +9,24 @@ interface ResultModalProps {
 
 export default function ResultModal({ gameState, onReturnToTitle }: ResultModalProps) {
   return (
-    <div>
-      {/* TODO: implement - final scores, win/loss display, return to title button */}
+
+    
+    <div className="bg-gray-800/40 place-self-center text-center outline-double rounded-[20px] pb-3 ">
+      <div className="text-bg-10 text-3xl pt-10 mb-5 text-shadow-md text-shadow-slate-300">FINAL SCORES
+        </div>
+      <div className="flex place-content-center gap-5">
+        <div className="text-red-600 text-3xl  
+        text-shadow-md text-shadow-red-600">TEAM RED
+          <p className="text-9xl">10</p>
+          </div>
+        <div className="text-blue-500 text-3xl text-shadow-cyan-400 text-shadow-md">TEAM BLUE 
+          <p className="text-9xl">10</p>
+         </div>
+      </div>
+      <p className="animate-pulse text-5xl text-red-500 text-shadow-md text-shadow-red-600 font-bold mt-7 pb-10 px-10">RED TEAM WINS!</p> 
+      <button className="text-white-100 rounded-[40px] border-2 px-3 border-cyan-100/70 text-shadow-md text-blue-red-100">ロビーに戻る
+      </button>
     </div>
+
   );
 }
