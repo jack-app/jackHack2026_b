7e36035675086687cb61548f28949c97e8f1f1af
SHAOYIK YEOH <sooh.ooo@gmail.com>
2026-05-04 11:48:14 +0900

    style/centerlize item and change background color

--- switch_game/src/components/Map/Item.tsx
+++ switch_game/src/components/Map/Item.tsx
@@ -11,7 +11,7 @@ export interface ItemProps {
 
 export default function Item(_props: ItemProps) {
   if (_props.type === "reverse") {
-    return <div className="w-10 h-10">
+    return <div className="w-8 h-8">
       <Image
       src="/item_arrow_2.png"
       width={100}
@@ -22,7 +22,7 @@ export default function Item(_props: ItemProps) {
   }
 
   if (_props.type === "blinded") {
-    return <div className="w-10 h-10">
+    return <div className="w-8 h-8">
       <Image
       src="/item_blined.png"
       width={100}
@@ -33,7 +33,7 @@ export default function Item(_props: ItemProps) {
   }
 
   if (_props.type === "jump") {
-    return <div className="w-10 h-10" >
+    return <div className="w-8 h-8" >
       <Image
       src="/item_jump.png"
       width={100}
